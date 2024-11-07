f3d699fae13580aec11e50766d50b4f8834ec047
Finn Henninghaus <finn.henninghaus@gmail.com>
2024-11-07 23:59:11 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { createGlobalStyle } from 'styled-components';
 
 
 const GlobalStyle = createGlobalStyle`
-  font-family: sans-serif;
+  
   ::selection {
-    background-color: #639A00; /* Change to your desired highlight color */
-    color: #1b1e23; /* Change to your desired text color */
+    background-color: #ffffff; /* Change to your desired highlight color */
+    color: #000000; /* Change to your desired text color */
   }
 
   /* For Firefox */
   ::-moz-selection {
-    background-color: #639A00; /* Change to your desired highlight color */
-    color: #1b1e23; /* Change to your desired text color */
+    background-color: #ffffff; /* Change to your desired highlight color */
+    color: #000000; /* Change to your desired text color */
   }
 `;
 

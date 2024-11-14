95b18e2332fd8218af15a9f7ec24648f812bce2b
Z-421 <khazaeireza97@gmail.com>
2024-11-14 17:16:59 +0100

    checking the connection betwen database and main.c

--- clogic/main.c
+++ clogic/main.c
@@ -1,11 +1,31 @@
+#include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(){
 
+    MYSQL *connection;
+    connection = mysql_init(NULL);
 
-int main() {
+    const char *password = getenv("PASS");
+    if (password == NULL){
+        printf("getenv returned NULL\n");
+        exit(1);
+    }
+
+    if (connection == NULL)
+    {
+        printf("ERROR %u %s\n", mysql_errno(connection), mysql_error(connection));
+        exit(1);
+    }else if (mysql_real_connect(connection,"minicloud-db-minicloud-db.j.aivencloud.com", "avnadmin", password, "mysql", 0, NULL, 0)==NULL){
+        printf("mysql_real_connect() failed\n");
+        mysql_close(connection);
+        exit(1);
+    }
+
+    printf("NICE");
+    mysql_close(connection);
     
-    printf("Hello Wrold!");
+
     return 0;
 }
-
